07a206455f308d9f969bcbe7baa120d7be52e227
Ian Velazquez <ian.codingmakers@gmail.com>
2024-07-23 17:45:42 -0600

    fix: fixed timezones

--- app/repositories/OfferRepository.py
+++ app/repositories/OfferRepository.py
@@ -17,7 +17,47 @@ def find(filters):
 
 
 def find_by_request_id_and_group(request_id: str, group_id: str):
-    return database.db["Offers"].find({"request_id": request_id, "group_id": group_id})
+    return database.db["Offers"].aggregate([
+        {
+            "$match": {"request_id": request_id, "group_id": group_id}
+        },
+        {
+            "$lookup": {
+                "from": "Users",
+                "localField": "user_uid",
+                "foreignField": "uid",
+                "as": "user_info"
+            }
+        },
+        {
+            "$unwind": {
+                "path": "$user_info",
+                "preserveNullAndEmptyArrays": True
+            }
+        },
+        {
+            "$project": {
+                "_id": 1,
+                "request_id": 1,
+                "user_uid": 1,
+                "group_id": 1,
+                "brand": 1,
+                "guarantee": 1,
+                "price": 1,
+                "unit_of_measure": 1,
+                "to_be_delivered_time": 1,
+                "code": 1,
+                "location": 1,
+                "photos": 1,
+                "internalComments": 1,
+                "publicComments": 1,
+                "status": 1,
+                "type": 1,
+                "user_info.uid": 1,
+                "user_info.name": 1,
+            }
+        }
+    ])
 
 
 def find_by_request_ids(request_ids):
