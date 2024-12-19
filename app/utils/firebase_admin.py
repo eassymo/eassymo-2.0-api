4ecd02d7137aa2915a42e09268164bd6e27d8ad5
Ian Velazquez <ian.codingmakers@gmail.com>
2024-12-19 15:25:11 +0200

    feat: added middleware for firebase auth

--- app/utils/firebase_admin.py
+++ app/utils/firebase_admin.py
@@ -4,8 +4,21 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-current_dir = os.path.dirname(os.path.abspath(__file__))
-cert_path = os.path.join(current_dir, "eassymo-416717-firebase-adminsdk-nk1q5-9e25e0f01c.json")
 
-cred = credentials.Certificate(cert_path)
+# Create credentials dictionary from environment variables
+cred_dict = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),  # Fix private key formatting
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
+
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
