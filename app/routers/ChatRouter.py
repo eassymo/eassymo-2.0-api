3792adfa49059cd1490264a4711883e9e52107e7
Ian Velazquez <ian.codingmakers@gmail.com>
2026-04-02 19:38:21 -0600

    fix: fixed chat to be read

--- app/routers/ChatRouter.py
+++ app/routers/ChatRouter.py
@@ -36,10 +36,17 @@ def find_by_request_or_order_id(id: str = Query(None, title="id"), type: str = Q
         return JSONResponse(content=get_unsuccessful_response(e))
 
 
-@chatRouter.get('/to-be-read/{id}', response_description="Integer number of the messages that still need to be read", tags=["Chat"])
-def to_be_read(id: str, type: str = Query(None, title="type"), userUid: str = Query(None, title="userUid")):
+@chatRouter.post('/to-be-read', response_description="Object with id of request and pending messages", tags=["Chat"])
+def to_be_read(body=Body(...)):
     try:
-        response = chatService.to_be_read(id, userUid, type)
+
+        ids = body.get("request_ids")
+
+        if (len(ids) == 0 or ids is None):
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=get_unsuccessful_response("Ids cannot be empty"))
+
+        response = chatService.to_be_read_v2(ids)
+        
         return JSONResponse(status_code=status.HTTP_200_OK, content=get_successful_response(jsonable_encoder(response)))
     except Exception as e:
         return JSONResponse(content=get_unsuccessful_response(e))
